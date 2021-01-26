5a4d4504d4d0a3880b40537fafe059599ae9f59b
rowan2702 <rowankbaby@gmail.com>
2021-01-26 10:17:59 +0530

    new commit

--- ml_backend/endpoint/views.py
+++ ml_backend/endpoint/views.py
@@ -1,6 +1,16 @@
+
 from django.shortcuts import render
+from .models import Director, Producer, Narrative, Hero, Actor, Song
 
 # Create your views here.
 
 def home(request) :
-    return render(request,'templates/index.html')
+    context = { 
+        'directors' : Director.objects.all(),
+        'producers' : Producer.objects.all(),
+        'narratives' : Narrative.objects.all(),
+        'heroes' : Hero.objects.all(),
+        'actors' : Actor.objects.all(),
+        'songs' : Song.objects.all()
+    }
+    return render(request,'index.html',context)
